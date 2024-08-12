c70814827d356b8e8948cdd29a901fe47e3bd554
Vladyslav Voronin <157734203+Vraznos@users.noreply.github.com>
2024-08-12 14:05:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-react-hw-03/",
   plugins: [react()],
   build: {
     sourcemap: true,
