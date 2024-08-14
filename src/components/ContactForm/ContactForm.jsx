265e11b8cb59bc6c439e2b0dea7f7ecca0d92a88
Vladyslav Voronin <157734203+Vraznos@users.noreply.github.com>
2024-08-14 13:04:58 +0200

    fixed mistakes

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -22,14 +22,12 @@ export default function ContactForm({ onAdd }) {
   const numberId = useId();
 
   const initValue = {
-    id: nanoid(),
     name: "",
     number: "",
   };
 
   const handleSubmit = (values, actions) => {
-    console.log(values);
-    onAdd(values);
+    onAdd({ ...values, id: nanoid() });
     actions.resetForm();
   };
 
